47d83f33f5fe8b5611e175e466d73913d16c7760
EstivenO <estivenortiz24@gmail.com>
2026-05-14 17:13:47 -0500

    se inicia la itegracion a Js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log("Hola");
+// ==========================
+// Selección de elementos DOM
+// ==========================
+
+const inputMateria = document.querySelector("#inputMateria");
+const inputCalificacion = document.querySelector("#inputCalificacion");
+const btnAgregarMateria = document.querySelector("#btnAgregar");
+const listaMaterias = document.querySelector("#listaMaterias");
+const mensajeValidacion = document.querySelector("#mensaje");
+
+// ==========================
+// Estado de la aplicación
+// ==========================
+
+const materias = [];
+
+
+
